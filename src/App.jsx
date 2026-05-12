db1987127b6e8bd001087a1150b5dac008d5ba4b
akshita <akshitasondagar81@gmail.com>
2026-05-12 18:46:29 +0530

    navbar working...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { useState } from "react";
+
+import { AdminSidebar } from "@/components/Navbar/AdminSidebar";
+import { AdminTopbar } from "@/components/Navbar/AdminTopBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false);
 
   return (
-    <>
-      <div className='flex items-center justify-center'>Hello world</div>
-    </>
-  )
+    <BrowserRouter>
+      <div className="flex bg-[#f5f7fb] min-h-screen">
+
+        <AdminSidebar
+          open={open}
+          setOpen={setOpen}
+        />
+
+        <div className="flex-1 flex flex-col md:ml-0">
+
+          <AdminTopbar setOpen={setOpen} />
+
+          <main className="p-4 md:p-6">
+            <Routes>
+
+              {/* <Route
+                path="/"
+                element={
+                  <Navigate to="/admin/overview" />
+                }
+              /> */}
+
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

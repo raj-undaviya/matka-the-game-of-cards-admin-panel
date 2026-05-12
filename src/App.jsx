e023e6b330c367bebf912178cd3c195d36cb3b3f
akshita <akshitasondagar81@gmail.com>
2026-05-12 15:58:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex items-center justify-center'>Hello world</div>
+    </>
+  )
+}
+
+export default App
